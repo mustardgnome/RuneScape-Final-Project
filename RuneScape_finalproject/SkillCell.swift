afa56247aaa46787f96531aec6856949bc41468c
mustardgnome <cole@mustardgno.me>
2020-05-10 19:22:52 -0500

    set up functionality for skills table

---
+++ RuneScape_finalproject/SkillCell.swift
@@ -0,0 +1,18 @@
+//
+//  SkillCell.swift
+//  RuneScape_finalproject
+//
+//  Created by Cole Jones on 5/10/20.
+//  Copyright © 2020 Allan Clarke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SkillCell: UITableViewCell {
+    @IBOutlet weak var skillName: UILabel!
+    @IBOutlet weak var skillLevel: UILabel!
+    @IBOutlet weak var currentXP: UILabel!
+    @IBOutlet weak var skillImage: UIImageView!
+    
+}
--- RuneScape_finalproject/SkillsTableVC.swift
+++ RuneScape_finalproject/SkillsTableVC.swift
@@ -15,9 +15,39 @@ class SkillsTableVC: UITableViewController {
     
     var player : NSManagedObject?
     
+    var skillNames : Array<String>?
+
     override func viewDidLoad() {
         print(player!.value(forKey: "name")!)
+//        player!.value(forKey: "")
+        skillNames = ["overall","attack","defence","strength","hitpoints","ranged","prayer","magic","cooking"
+        ,"woodcutting","fletching","fishing","firemaking","crafting","smithing","mining","herblore","agility"
+            ,"thieving","slayer","farming","runecrafting","hunter","construction"]
+        
+        
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return skillNames!.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "skillCell", for: indexPath) as! SkillCell
+
+        print(skillNames![indexPath.row])
+        let level = player!.value(forKey: skillNames![indexPath.row])
+        let currXP = player!.value(forKey: (skillNames![indexPath.row]+"XP"))
+        
+        cell.skillName.text = skillNames![indexPath.row].uppercased()
+        cell.skillLevel.text = "\(level!)"
+        cell.currentXP.text = "\(currXP!)"
+        
+
+        return cell
+    }
     
 }
